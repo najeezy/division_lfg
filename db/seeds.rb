cb2c0372cd0d2e617ea99dbc5d24cd73ad9003d9
truotaku <najee.s.gardner@gmail.com>
2016-03-28 14:28:03 -0400

    added random users to the seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ users = [
     player: Player.create({
       platform: 'ps4',
       username: 'TruO_',
-      level: 23,
+      level: (1..30).to_a.sample,
       has_mic: true,
       created_groups: Group.create([
         { mission_name: 'Madision Square Hospital', is_active: true }
@@ -28,7 +28,7 @@ users = [
     player: Player.create({
       platform: 'ps4',
       username: 'Rinzer_',
-      level: 25,
+      level: (1..30).to_a.sample,
       has_mic: true,
       created_groups: Group.create([
         { mission_name: 'Darkzone Patrol', is_active: true }
@@ -42,7 +42,21 @@ users = [
     player: Player.create({
       platform: 'ps4',
       username: 'BalCuatro',
-      level: 13,
+      level: (1..30).to_a.sample,
+      has_mic: false,
+      created_groups: Group.create([
+        { mission_name: 'Base of Ops', is_active: true }
+      ])
+    })
+  },
+  {
+    email: 'criscorprew@gmail.com',
+    password: 'Abcd1234',
+    password_confirmation: 'Abcd1234',
+    player: Player.create({
+      platform: 'ps4',
+      username: 'Copastetik',
+      level: (1..30).to_a.sample,
       has_mic: false,
       created_groups: Group.create([
         { mission_name: 'Base of Ops', is_active: true }
@@ -51,10 +65,28 @@ users = [
   }
 ]
 
-User.create(users)
+def fake_user_data
+  {
+    email: Faker::Internet.email,
+    password: 'Abcd1234',
+    password_confirmation: 'Abcd1234',
+    player: Player.create({
+      platform: ['ps4', 'xb1'].sample,
+      username: Faker::Internet.user_name,
+      level: (1..30).to_a.sample,
+      has_mic: [true, false].sample,
+      created_groups: Group.create([
+        { mission_name: Faker::Lorem.sentence, is_active: true }
+      ])
+    })
+  }
+end
 
-first_group = Group.first
-last_group = Group.last
+fake_users = (1..30).to_a.map { |num| fake_user_data }
 
-first_group.players << Player.where("id <> #{first_group.creator.id}")
-last_group.players << Player.first
+User.create(users + fake_users)
+
+# create random group/player associations
+Group.all.each do |group|
+  group.players << Player.where("id <> #{group.creator.id}").order('RANDOM()').limit([0,1,2].sample)
+end
